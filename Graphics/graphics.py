09a5c96462faddf1eef738f7ab288840bfe9c30e
turkish <turkish@d4rkwolf.com>
2024-05-20 21:40:48 +0100

    Graphics update

--- Graphics/graphics.py
+++ Graphics/graphics.py
@@ -3,14 +3,22 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import numpy as np
+import time
 
-# Define colors
-RED = (1.0, 0.0, 0.0)
-PURPLE = (0.5, 0.0, 0.5)
-BLUE = (0.0, 0.0, 1.0)
+# Define colors for transitions
+COLORS = [
+    np.array([1.0, 0.0, 0.0]),  # Red
+    np.array([0.5, 0.0, 0.5]),  # Purple
+    np.array([0.0, 0.0, 1.0])   # Blue
+]
+
+# Interpolate between two colors
+def interpolate_color(color1, color2, t):
+    return color1 * (1 - t) + color2 * t
 
 # Draw sphere
-def draw_sphere(radius=1.0, slices=30, stacks=30):
+def draw_sphere(radius=1.0, slices=40, stacks=40):
     quadric = gluNewQuadric()
     gluQuadricNormals(quadric, GLU_SMOOTH)
     gluSphere(quadric, radius, slices, stacks)
@@ -27,11 +35,13 @@ def init_lighting():
     glLightfv(GL_LIGHT0, GL_DIFFUSE, [1, 1, 1, 1])
     glLightfv(GL_LIGHT0, GL_SPECULAR, [1, 1, 1, 1])
     glLightfv(GL_LIGHT0, GL_POSITION, [1, 1, 1, 0])
+    glEnable(GL_LIGHT0)
 
     # Light 1
     glLightfv(GL_LIGHT1, GL_DIFFUSE, [0.5, 0.5, 0.5, 1])
     glLightfv(GL_LIGHT1, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
     glLightfv(GL_LIGHT1, GL_POSITION, [-1, -1, -1, 0])
+    glEnable(GL_LIGHT1)
 
 # Initialize material properties
 def init_material():
@@ -41,23 +51,67 @@ def init_material():
     glMaterialfv(GL_FRONT, GL_SHININESS, [100])
 
 # Mouse control for interactive rotation and zoom
-def mouse_control():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 4:
-                glTranslatef(0, 0, 1)
-            elif event.button == 5:
-                glTranslatef(0, 0, -1)
-            elif event.button == 1:
-                x, y = event.pos
-                width, height = pygame.display.get_surface().get_size()
-                normalized_x = (2.0 * x - width) / width
-                normalized_y = (height - 2.0 * y) / height
-                glRotatef(30 * normalized_x, 0, 1, 0)
-                glRotatef(30 * normalized_y, 1, 0, 0)
+rotation_speed = 0.5
+zoom_speed = 0.1
+current_angle = [0, 0]
+
+def handle_mouse():
+    global current_angle
+    mouse_state = pygame.mouse.get_pressed()
+    mouse_rel = pygame.mouse.get_rel()
+
+    if mouse_state[0]:
+        current_angle[0] += mouse_rel[1] * rotation_speed
+        current_angle[1] += mouse_rel[0] * rotation_speed
+
+    keys = pygame.key.get_pressed()
+    if keys[K_UP]:
+        glTranslatef(0, 0, zoom_speed)
+    if keys[K_DOWN]:
+        glTranslatef(0, 0, -zoom_speed)
+
+# GLSL Shaders
+vertex_shader_source = """
+#version 130
+in vec4 position;
+in vec3 normal;
+out vec3 frag_normal;
+void main() {
+    gl_Position = gl_ModelViewProjectionMatrix * position;
+    frag_normal = normal;
+}
+"""
+
+fragment_shader_source = """
+#version 130
+in vec3 frag_normal;
+out vec4 frag_color;
+uniform vec3 light_color;
+void main() {
+    float intensity = max(dot(normalize(frag_normal), vec3(0, 0, 1)), 0.0);
+    frag_color = vec4(light_color * intensity, 1.0);
+}
+"""
+
+def compile_shader(source, shader_type):
+    shader = glCreateShader(shader_type)
+    glShaderSource(shader, source)
+    glCompileShader(shader)
+    if not glGetShaderiv(shader, GL_COMPILE_STATUS):
+        raise RuntimeError(glGetShaderInfoLog(shader))
+    return shader
+
+def init_shaders():
+    vertex_shader = compile_shader(vertex_shader_source, GL_VERTEX_SHADER)
+    fragment_shader = compile_shader(fragment_shader_source, GL_FRAGMENT_SHADER)
+    shader_program = glCreateProgram()
+    glAttachShader(shader_program, vertex_shader)
+    glAttachShader(shader_program, fragment_shader)
+    glLinkProgram(shader_program)
+    if not glGetProgramiv(shader_program, GL_LINK_STATUS):
+        raise RuntimeError(glGetProgramInfoLog(shader_program))
+    glUseProgram(shader_program)
+    return shader_program
 
 def main():
     pygame.init()
@@ -68,24 +122,42 @@ def main():
     init_lighting()
     init_material()
 
+    shader_program = init_shaders()
+    light_color_location = glGetUniformLocation(shader_program, "light_color")
+
+    clock = pygame.time.Clock()
+
+    start_time = time.time()
+    color_index = 0
+
     while True:
-        mouse_control()
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        handle_mouse()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        # Draw spheres with different colors
-        glColor3fv(RED)
-        draw_sphere()
+        # Color transition
+        elapsed_time = time.time() - start_time
+        t = (elapsed_time % 2) / 2.0
+        next_color_index = (color_index + 1) % len(COLORS)
+        current_color = interpolate_color(COLORS[color_index], COLORS[next_color_index], t)
+        if elapsed_time > 2:
+            start_time = time.time()
+            color_index = next_color_index
 
-        glTranslatef(3, 0, 0)
-        glColor3fv(PURPLE)
+        # Rotate sphere
+        glPushMatrix()
+        glRotatef(current_angle[0], 1, 0, 0)
+        glRotatef(current_angle[1], 0, 1, 0)
+        glUniform3fv(light_color_location, 1, current_color)
         draw_sphere()
-
-        glTranslatef(-6, 0, 0)
-        glColor3fv(BLUE)
-        draw_sphere()
+        glPopMatrix()
 
         pygame.display.flip()
-        pygame.time.wait(10)
 
 if __name__ == "__main__":
     main()
