0dc0ab8839ce7a667d9f2af509cccf78190d7920
Tukish <Tukish@d4rkwolf.com>
2024-05-21 09:01:26 +0100

    Graphics update

--- Graphics/graphics.py
+++ Graphics/graphics.py
@@ -13,66 +13,88 @@ PURPLE_SHADES = [
     np.array([0.6, 0.0, 0.8]),  # Dark Vibrant Purple
 ]
 
-# Interpolate between two colors
 def interpolate_color(color1, color2, t):
+    """Interpolate between two colors."""
     return color1 * (1 - t) + color2 * t
 
-# Initialize particle system
-def generate_particles(num_particles=10000):
-    particles = []
-    for _ in range(num_particles):
-        phi = random.uniform(0, 2 * np.pi)
-        theta = random.uniform(0, np.pi)
-        x = np.sin(theta) * np.cos(phi)
-        y = np.sin(theta) * np.sin(phi)
-        z = np.cos(theta)
-        particles.append([x, y, z])
-    return particles
+class ParticleSystem:
+    def __init__(self, num_particles=10000):
+        self.particles = self.generate_particles(num_particles)
+        self.floating_particles = self.generate_floating_particles(num_particles // 2)
 
-def generate_floating_particles(num_particles=5000):
-    floating_particles = []
-    for _ in range(num_particles):
-        x, y, z = np.random.uniform(-3, 3, 3)
-        floating_particles.append([x, y, z])
-    return floating_particles
+    def generate_particles(self, num_particles):
+        particles = []
+        for _ in range(num_particles):
+            phi = random.uniform(0, 2 * np.pi)
+            theta = random.uniform(0, np.pi)
+            x = np.sin(theta) * np.cos(phi)
+            y = np.sin(theta) * np.sin(phi)
+            z = np.cos(theta)
+            particles.append([x, y, z])
+        return particles
 
-def draw_particles(particles, color):
-    glBegin(GL_POINTS)
-    glColor3f(*color)
-    for p in particles:
-        glVertex3f(p[0], p[1], p[2])
-    glEnd()
+    def generate_floating_particles(self, num_particles):
+        floating_particles = []
+        for _ in range(num_particles):
+            x, y, z = np.random.uniform(-3, 3, 3)
+            floating_particles.append([x, y, z])
+        return floating_particles
 
-def draw_floating_particles(particles, color):
-    glBegin(GL_POINTS)
-    for p in particles:
-        color = np.random.rand(3)
+    def draw_particles(self, particles, color):
+        glBegin(GL_POINTS)
         glColor3f(*color)
-        glVertex3f(p[0], p[1], p[2])
-    glEnd()
+        for p in particles:
+            glVertex3f(p[0], p[1], p[2])
+        glEnd()
 
-# Initialize lighting and material properties
-def init_lighting_and_material():
-    glEnable(GL_LIGHTING)
-    glEnable(GL_DEPTH_TEST)
-    glEnable(GL_NORMALIZE)
-    glShadeModel(GL_SMOOTH)
+    def draw_floating_particles(self, particles):
+        glBegin(GL_POINTS)
+        for p in particles:
+            color = np.random.rand(3)
+            glColor3f(*color)
+            glVertex3f(p[0], p[1], p[2])
+        glEnd()
 
-    # Light 0
-    glLightfv(GL_LIGHT0, GL_AMBIENT, [0.2, 0.2, 0.2, 1])
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, [1, 1, 1, 1])
-    glLightfv(GL_LIGHT0, GL_SPECULAR, [1, 1, 1, 1])
-    glLightfv(GL_LIGHT0, GL_POSITION, [5, 5, 5, 1])
-    glEnable(GL_LIGHT0)
+class Lighting:
+    def __init__(self):
+        self.setup_lighting()
 
-    # Light 1
-    glLightfv(GL_LIGHT1, GL_AMBIENT, [0.2, 0.2, 0.2, 1])
-    glLightfv(GL_LIGHT1, GL_DIFFUSE, [0.5, 0.5, 0.5, 1])
-    glLightfv(GL_LIGHT1, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
-    glLightfv(GL_LIGHT1, GL_POSITION, [-5, -5, 5, 1])
-    glEnable(GL_LIGHT1)
+    def setup_lighting(self):
+        glEnable(GL_LIGHTING)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_NORMALIZE)
+        glShadeModel(GL_SMOOTH)
 
-# Main loop
+        # Light 0
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [0.2, 0.2, 0.2, 1])
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, [1, 1, 1, 1])
+        glLightfv(GL_LIGHT0, GL_SPECULAR, [1, 1, 1, 1])
+        glLightfv(GL_LIGHT0, GL_POSITION, [5, 5, 5, 1])
+        glEnable(GL_LIGHT0)
+
+        # Light 1
+        glLightfv(GL_LIGHT1, GL_AMBIENT, [0.2, 0.2, 0.2, 1])
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, [0.5, 0.5, 0.5, 1])
+        glLightfv(GL_LIGHT1, GL_SPECULAR, [0.5, 0.5, 0.5, 1])
+        glLightfv(GL_LIGHT1, GL_POSITION, [-5, -5, 5, 1])
+        glEnable(GL_LIGHT1)
+
+class TimeManager:
+    def __init__(self, transition_time=4):
+        self.start_time = time.time()
+        self.color_index = 0
+        self.transition_time = transition_time
+
+    def update_color(self, shades):
+        elapsed_time = time.time() - self.start_time
+        t = (elapsed_time % self.transition_time) / self.transition_time
+        next_color_index = (self.color_index + 1) % len(shades)
+        current_color = interpolate_color(shades[self.color_index], shades[next_color_index], t)
+        if elapsed_time > self.transition_time:
+            self.start_time = time.time()
+            self.color_index = next_color_index
+        return current_color
+
 def main():
     pygame.init()
 
@@ -84,16 +106,11 @@ def main():
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-    init_lighting_and_material()
+    lighting = Lighting()
+    time_manager = TimeManager()
+    particle_system = ParticleSystem()
 
     clock = pygame.time.Clock()
-
-    start_time = time.time()
-    color_index = 1
-    transition_time = 4  # seconds
-
-    particles = generate_particles()
-    floating_particles = generate_floating_particles()
 
     while True:
         for event in pygame.event.get():
@@ -110,25 +127,17 @@ def main():
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glClearColor(0.0, 0.0, 0.0, 0.0)  # Transparent background
 
-        # Color transition
-        elapsed_time = time.time() - start_time
-        t = (elapsed_time % transition_time) / transition_time
-        next_color_index = (color_index + 1) % len(PURPLE_SHADES)
-        current_color = interpolate_color(PURPLE_SHADES[color_index], PURPLE_SHADES[next_color_index], t)
-        if elapsed_time > transition_time:
-            start_time = time.time()
-            color_index = next_color_index
+        current_color = time_manager.update_color(PURPLE_SHADES)
 
-        # Rotate and draw particles
         glPushMatrix()
+        elapsed_time = time.time() - time_manager.start_time
         glRotatef(elapsed_time * 20, 1, 1, 0)
-        draw_particles(particles, current_color)
+        particle_system.draw_particles(particle_system.particles, current_color)
         glPopMatrix()
 
-        # Draw floating particles
         glPushMatrix()
         glRotatef(elapsed_time * 10, 0, 1, 0)
-        draw_floating_particles(floating_particles, current_color)
+        particle_system.draw_floating_particles(particle_system.floating_particles)
         glPopMatrix()
 
         pygame.display.flip()
